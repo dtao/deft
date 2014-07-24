7e0ad4155aefb54c20cd9147302eebdd5a1a28da
Dan Tao <daniel.tao@gmail.com>
2014-07-23 19:41:39 -0700

    fixed up annotations (all 2 of them) in deft.js

--- deft.js
+++ deft.js
@@ -1,13 +1,16 @@
 /**
+ * Deft: a JavaScript dependency declaration system.
+ */
+
+/**
  * Clips a string to a given maximum length.
  *
- * @private
  * @examples
- * clip('hello', 10); // => 'hello'
- * clip('hello', 4);  // => 'h...'
- * clip('hello', -1); // => '...'
+ * deft.clip('hello', 10); // => 'hello'
+ * deft.clip('hello', 4);  // => 'h...'
+ * deft.clip('hello', -1); // => '...'
  */
-function clip(str, length) {
+exports.clip = function clip(str, length) {
   length = Math.max(length, 3);
 
   if (str.length <= length) {
@@ -15,26 +18,20 @@ function clip(str, length) {
   }
 
   return str.substring(0, length - 3) + '...';
-}
+};
 
 /**
  * Negates a function; i.e., returns whatever the opposite result is (assumes
  * boolean return values).
  *
- * @private
  * @examples
  * var returnTrue = function() { return true; };
  *
- * not(returnTrue);   // instanceof Function
- * not(returnTrue)(); // => false
+ * deft.not(returnTrue);   // instanceof Function
+ * deft.not(returnTrue)(); // => false
  */
-function not(fn) {
+exports.not = function not(fn) {
   return function() {
     return !fn.apply(this, arguments);
   };
-}
-
-module.exports = {
-  clip: clip,
-  not: not
 };
